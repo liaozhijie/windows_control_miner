2c700a32be84b384002f4c129f75b5b173a6666b
liaozhijie <36397435+liaozhijie@users.noreply.github.com>
2021-11-26 18:07:11 +0800

    Update base_control.py

--- base_control.py
+++ base_control.py
@@ -5,19 +5,21 @@ import download_config_apply
 import miner
 import monitor
 import send_email
+import sys
 
 CONFIG_PATH = 'C:/github/windows_control_miner-main/config.txt'
 DOWNLOAD_GAP_TIME = 600
 machaine = '1'
-CONFIG_DICT = download_config_apply.get_config_data(CONFIG_PATH)
 
 if __name__ == '__main__':
-    send_email.send_email("open compute", "open compute", 0, 3)
-    download_config_apply.download()
-    download_config_apply.apply_operation()
-    miner.start_mining(0, CONFIG_DICT)
-    monitor.start_monitor(0, CONFIG_DICT)
-    send_email.send_email("start mining", "start mining", 0, 3)
+    #send_email.send_email("open compute", "open compute", 0, 3)
+    download_config_apply.download(1)
+    #download_config_apply.apply_operation()
+    #CONFIG_DICT = download_config_apply.get_config_data(CONFIG_PATH)
+    #miner.start_mining(0, CONFIG_DICT)
+    #monitor.start_monitor(0, CONFIG_DICT)
+    #send_email.send_email("start mining", "start mining", 0, 3)
+    sys.exit(-1)
     while True:
         time.sleep(DOWNLOAD_GAP_TIME)
         download_config_apply.download()
