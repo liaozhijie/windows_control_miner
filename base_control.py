f2be1af940b98babff246aea320d6ebc3e03cfe8
liaozhijie <36397435+liaozhijie@users.noreply.github.com>
2021-11-27 15:14:16 +0800

    Update base_control.py

--- base_control.py
+++ base_control.py
@@ -13,13 +13,11 @@ machaine = '1'
 
 if __name__ == '__main__':
     #send_email.send_email("open compute", "open compute", 0, 3)
-    #download_config_apply.download(1)
+    download_config_apply.download(1)
     CONFIG_DICT = download_config_apply.get_config_data(CONFIG_PATH)
     #miner.start_mining(0, CONFIG_DICT)
     monitor.start_monitor(0, CONFIG_DICT)
     #send_email.send_email("start mining", "start mining", 0, 3)
-    sys.exit(-1)
     while True:
         time.sleep(DOWNLOAD_GAP_TIME)
-        download_config_apply.download()
-        download_config_apply.apply_operation()
+        download_config_apply.download(1)
