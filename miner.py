3560589066aa51e230f710da69e12afbd9ae0390
liaozhijie <36397435+liaozhijie@users.noreply.github.com>
2021-11-27 13:58:50 +0800

    Update miner.py

--- miner.py
+++ miner.py
@@ -8,14 +8,6 @@ FILE_PATH = 'C:/github/'
 def start_mining(if_restart, CONFIG_DICT):
     print ("start mining")
     miner_software, start_file_path = CONFIG_DICT['miner_info'].split(',')[:2]
-    if if_restart == 1 and '轻松旷工' not in miner_software:
-        try:
-            os.system(r'taskkill /F /IM cmd.exe')
-        except:
-            pass
-        time.sleep(30)
-    elif if_restart == 1 and '轻松旷工' in miner_software:
-        send_email.send_email("operation fail", "can not restart miner on qskg", 1, 3)
 
     os.chdir(FILE_PATH)
     if '轻松旷工' not in miner_software:
