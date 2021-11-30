bfb48a24c406e734ac0ab08ce22aa32fcea888b6
liaozhijie <36397435+liaozhijie@users.noreply.github.com>
2021-11-30 10:38:30 +0800

    Update base_control.py

--- base_control.py
+++ base_control.py
@@ -26,6 +26,5 @@ if __name__ == '__main__':
     
     #send_email.send_email("start mining", "start mining", 0, 3)
     while True:
-        print ("in while loop")
         time.sleep(DOWNLOAD_GAP_TIME)
         download_config_apply.download(1)
